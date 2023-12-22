77804880bc0b6986f1e9536f4279e5c18c95f9c1
mylongcode <mylongcode@gmail.com>
2023-12-22 20:59:39 +0500

    final commit

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 import './get-images.js';
 import './big-image.js';
 import './file-form.js';
+import './slider.js';
 import './image-effects.js';
 import './image-filters.js';
 import './validation.js';
